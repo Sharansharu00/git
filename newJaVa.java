2505934dd3adef001b5d53c0ee67e0767c2e183e
Sharansharu00 <sharan2252001@gmail.com>
2024-02-07 11:55:24 +0530

    update java file

---
+++ newJaVa.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class newJaVa{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        for (int i=0;i<=255;i++){
+            char Ascii=(char)i;
+            System.out.println(i+" Ascii values is: "+Ascii);
+        }
+
+    }
+}
